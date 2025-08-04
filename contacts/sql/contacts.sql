b81c0d335a618b372926bc716fc4751afb31833c
mstfkadum <mstfkadum@gmail.com>
2025-08-04 15:21:44 +0300

    names and Emailes Updated

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -3,11 +3,11 @@ drop table if exists contacts;
 create table contacts(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
 
 insert into contacts(firstname, lastname, email) values
-  ('Bilbo','Baggins','bilbo@theshire.com'),
-  ('Frodo','Baggins','frodo@theshire.com'),
-  ('Samwise','Gamgee','sam@theshire.com'),
-  ('Peregrin','Took','pippin@theshire.com'),
-  ('Meriadoc','Brandybuck','merry@theshire.com');
+  ('mustafa','abdul','xyz@xyz.com'),
+  ('yousif','Rayed','xyz@xyz.com'),
+  ('Mahdi','Adnan','xyz@xyz.com'),
+  ('Hadi','Sameer','xyz@xyz.com'),
+  ('ahmed','twerej','xyz@xyz.com');
 
 
 
